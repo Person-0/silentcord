117c889c53748ad954509d9a776feb5d18d450a9
Person-0 <atulyajain28122007@gmail.com>
2025-12-11 18:30:10 +0530

    export schemas that will be used in socket schema

--- src/schemas/api.ts
+++ src/schemas/api.ts
@@ -3,10 +3,10 @@ import config from "../config";
 import { accessTokenLength } from "../modules/accounts";
 import { roomIDlength } from "../modules/room";
 
-const username = z.string().max(config.max_username_length).min(config.min_username_length);
-const accountPassword = z.string().max(config.max_password_length).min(config.min_password_length);
-const accessToken = z.string().length(accessTokenLength);
-const roomPasword = z.string().min(config.min_room_password_length).max(config.max_room_password_length);
+export const username = z.string().max(config.max_username_length).min(config.min_username_length);
+export const accountPassword = z.string().max(config.max_password_length).min(config.min_password_length);
+export const accessToken = z.string().length(accessTokenLength);
+export const roomPasword = z.string().min(config.min_room_password_length).max(config.max_room_password_length);
 
 export const signupRequest = z.strictObject({
     username,
